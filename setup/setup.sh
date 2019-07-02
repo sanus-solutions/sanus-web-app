c8fa9cc98f09817d6b7bde9a8b55645d898670db
Luka Soban <luka.antolics@gmail.com>
2019-07-02 17:46:04 -0400

    Finalized setup.sh to download, install, and move all folders and files around before starting druid services.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ..
 echo "unpacking Druid folder..."
+curl https://www-us.apache.org/dist/incubator/druid/0.15.0-incubating/apache-druid-0.15.0-incubating-bin.tar.gz -o apache-druid-0.15.0-incubating-bin.tar.gz
 tar -xzf apache-druid-0.15.0-incubating-bin.tar.gz
 mv apache-druid-0.15.0-incubating druid
 cd druid
@@ -14,4 +15,15 @@ mv zookeeper-3.4.11 zk
 curl http://static.druid.io/tranquility/releases/tranquility-distribution-0.8.3.tgz -o tranquility-distribution-0.8.3.tgz
 tar -xzf tranquility-distribution-0.8.3.tgz
 mv tranquility-distribution-0.8.3 tranquility
+
+## Move sanus-index.json into correct folder
+cd ../setup/
+cp sanus-index.json ../druid/conf/tranquility/
+cd ../
+cd druid
+pwd
+
+## Cleanup
+rm -rf tranquility-distribution-0.8.3.tgz
+rm -rf zookeeper-3.4.11.tar.gz
 

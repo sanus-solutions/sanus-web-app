4368f93c754b2c118e774e1c61ff82a26c2d10e5
Luka <luka.antolics@gmail.com>
2019-07-23 15:27:38 -0400

    Created Login and register modules. Saving for home.

--- sanus-app/src/app/app-routing.module.ts
+++ sanus-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: LoginComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+
+	// Otherwise direct to home
+	{ path: '*', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sanus-app/src/app/app.module.ts
+++ sanus-app/src/app/app.module.ts
@@ -6,18 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
+/* Material Design Imports */
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';  
+import {MatListModule} from '@angular/material/list';
+/*                          */
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';  
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

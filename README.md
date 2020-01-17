461c164f81ed5da674261d7ab7616464c222f845
Luka Soban <luka.antolics@gmail.com>
2020-01-17 18:45:19 -0500

    Added in symbols to Map for Staff and Children

--- README.md
+++ README.md
@@ -1,157 +1,17 @@
-# Sanus Web App
-Contains:
-* Druid
-* Superset
-* Web App
+# Sanus Solutions Daycare Web App
 
-## Druid and Superset Documentation
+## TODO
+* Redo the entire schema design to make it more relatable to a daycare environment (not enough time atm to complete this)
+* Implement Last Seen timestamp when a user clicks on a "room"
+* Make sure node server can run without MongoDB Running
+* Add in user photos
 
-### Druid Installation and Configuration
-
-#### Using the setup.sh script
-Go to the package root and go to the setup folder. Execute:
-```
-cd sanus-web-app/setup
-./setup.sh
-``` 
-If everything unpacks okay skip to the **ENABLE TRANQUILTY SERVER** section
-
-#### Manual Method
-
-Download from Apache
-
-```
-https://www-us.apache.org/dist/incubator/druid/0.15.1-incubating/apache-druid-0.15.1-incubating-bin.tar.gz
-```
-
-#### Install Zookeeper, Tranquility Server, and add Sanus Config files
-
-After you downloaded Druid, execute the ``` setup.sh ``` script in the setup folder. This will automatically download
-ZK and Tranquility and set up your folder structure.
-
-If the script doesn't work then follow the steps below:
-
-Go to the root druid directory and execute
-```
-curl https://archive.apache.org/dist/zookeeper/zookeeper-3.4.11/zookeeper-3.4.11.tar.gz -o zookeeper-3.4.11.tar.gz
-tar -xzf zookeeper-3.4.11.tar.gz
-mv zookeeper-3.4.11 zk
-```
-For Tranqulity:
-
-```
-curl http://static.druid.io/tranquility/releases/tranquility-distribution-0.8.3.tgz -o tranquility-distribution-0.8.3.tgz
-tar -xzf tranquility-distribution-0.8.3.tgz
-mv tranquility-distribution-0.8.3 tranquility
-```
-
-### Enable Tranquility Server
-
-In your 
-```druid/conf/supervise/single-server/micro-quickstart.conf```, uncomment the last line of code in the file.
-From here. change ```/conf/tranquility/wikipedia-index.json``` to ```/conf/tranquility/sanus-index.json```
-
-
-**Add Sanus Config Files**
-
-Go to the ```sanus-web-app/setup``` folder in the root of this repository and move the the ```sanus-index.json``` file to
-
-```
-/conf/tranquility/sanus-index.json
-```
-*IMPORTANT* You will have to go the micro-quickstart.conf file again to make sure tranquility has the path to the index file.
-
-### Starting Druid Services
-
-To start Druid firstly go to the root directory and execute 
-
-```
-./bin/start-micro-quickstart
-```
-
-or depending on the server configuration, look inside the /conf folder for other configs.
-
-Once the cluster has started, you can navigate to http://localhost:8888. The Druid router process, which serves the Druid
-console, resides at this address.
-
-### Tranquility
-
-Traquility server (HTTP Push) is automatically enabled in the configuration file. This is what we use to send data to druid instead of another service such as Kafka. If you need to change the ingestion spec it can be found in
-
-```
-/conf/tranquility/sanus-index.json
-```
-
-the ingestion spec is the file that determines how data should look like when inserted into Druid. The current spec contains these dimensions which are all strings:
-
-```
-"dimensionsSpec" : {
-                  "dimensions" : [
-                    "type",
-                    "nodeID",
-                    "staffID",
-                    "staff_title",
-                    "unit",
-                    "room_number",
-                    "response_type",
-                    "response_message"
-                  ]
-                }
-```
-
-### Testing Data Ingestion
-
-At this point Druid and Tranquility should be running so you can test an ingestion with
-
-
-```
-curl -XPOST -H'Content-Type: application/json' --data-binary @[data.json] http://localhost:8200/v1/post/[hospital]
-```
-
-You can change the variables in [ ] to your specific file and database name.
-
-
-
-## Superset installation and initialization
-
-Follow these few simple steps to install Superset:
-
-```
-# Install superset
-pip install superset
-
-# Create an admin user (you will be prompted to set a username, first and last name before setting a password)
-fabmanager create-admin --app superset
-
-# Initialize the database
-superset db upgrade
-
-# Load some data to play with
-superset load_examples
-
-# Create default roles and permissions
-superset init
-
-# To start a development web server on port 8088, use -p to bind to another port
-superset runserver -d
-
-```
-
-Installation Issues: 
-
-* Error with import maybe_box?
-
-```
-pip uninstall pandas
-pip list | grep pandas
-pip install pandas==0.23.4
-```
 
 # Schema Design
 
-We send to Druid data in JSON format. Each segment of data that is sent is considered an Event.
+We send to MongoDB in JSON format. Each segment of data that is sent is considered an Event.
 
-This is the format of an Event (note: they are all strings):
+This is the format of an Event sent to the server from the Camera Modules (note: they are all strings):
 ```
         {
           "time",
@@ -183,10 +43,10 @@ An example POST payload is:
 Legend:
 
 * time - ISO 8601 formated timestamp
-* type - The type of event. This can be entry, alert, dispeser
-* nodeID - The ID of the physical node placed in the hospital. This ID will have a specific coding to it so that you can find the Unit and Room Number from it.
+* type - The type of event. This can be entry or exit.
+* nodeID - The ID of the physical node placed in the daycare. This ID will correspond to a specific room number.
 * staffID - The ID associated with the specific staff member. This can be either a name or a specific number.
-* unit - This is the unit name within the hospital which will be derived from the coding of the nodeID (ex. ICU)
+* unit - This is the unit name within the daycare (ex. Kitchen)
 * room_number - This is the room number where the node resides. This is also derived from the nodeID (ex. RM301)
 * response_type - Type of response that is being given to user. This can be entry, alert, dispeser
 * response_message:
--- sanus-app/src/app/livemap/livemap.component.ts
+++ sanus-app/src/app/livemap/livemap.component.ts
@@ -25,6 +25,7 @@ export class LivemapComponent implements OnInit {
   Employee:any = [];
   CurrentRoom:any;
   CurrentRoomPeopleList: any;
+  RoomList:any;
   LastLocations:any = [];
 
   //subscriptions
@@ -35,6 +36,7 @@ export class LivemapComponent implements OnInit {
     this.LastLocations = new Array();
     this.CurrentRoom = null;
     this.CurrentRoomPeopleList = new Array();
+    this.RoomList = new Array();
     
   }
 
@@ -45,10 +47,12 @@ export class LivemapComponent implements OnInit {
     this.subscriptionToLocations = source.subscribe(val => this.refreshMap());
     
 
-    var project1 = new Project('cv1');
+    const project1 = new Project('cv1');
     var tool = new Tool();
 
     this.createDaycareLayout();
+
+    console.log(paper.project.layers);
 
     var hitOptions = {
       segments: true,
@@ -94,128 +98,235 @@ export class LivemapComponent implements OnInit {
       }
     }
 
+  }
+
+  /**
+    * Loop through each room and find the totals of each type in the map legends.
+    * From here, generate a number and the symbol depending on the type and how many of each in the room.
+    */
+  drawPeopleOnMap() {
+
+    // create a new layer and make it the active layer
+    if(paper.project.layers.length == 1) {
+
+      var layer2 = new Layer();
+      layer2.activate();
+
+      this.initForDrawPeopleOnMap();
+
+
+    } else {
+
+      // clear the 2nd layer and redraw it
+      paper.project.layers[1].removeChildren();
+      paper.project.layers[1].activate();
+      
+      this.initForDrawPeopleOnMap();
+
+    }
+    
+  }
+
+  /**
+   * Draws the symbols for Staff and Children onto the Map
+   */
+  initForDrawPeopleOnMap() {
+
+    for (var room of this.RoomList) {
+      var point = room.startPoint;
+
+      // create staff symbols on map
+      if (room.numOfStaff > 0) {
+        var staffPoint = new Point(point.x + 75, point.y + 50);
+        // create a green circle and a pointText number inside
+        var staffCircle = new Path.Circle(staffPoint, 15);
+        staffCircle.fillColor = new Color('green');
+        staffCircle.locked = true;
+
+        // create pointText based on number of staff in room
+        var text = new PointText({
+          point: [staffPoint.x-5,staffPoint.y+5],
+          content: room.numOfStaff.toString(),
+          fillColor: 'white',
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+          fontSize: 15,
+          locked: true
+        });
+      }
+
+      // create the child symbols on the map
+      if (room.numOfChildren > 0) {
+        var childPoint = new Point(point.x + 75, point.y + 100);
+        // create a blue circle and a pointText number inside
+        var childCircle = new Path.Circle(childPoint, 15);
+        childCircle.fillColor = new Color('blue');
+        childCircle.locked = true;
+
+        // create pointText based on number of staff in room
+        var text = new PointText({
+          point: [childPoint.x-5,childPoint.y+5],
+          content: room.numOfChildren.toString(),
+          fillColor: 'white',
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+          fontSize: 15,
+          locked: true
+        });
+      }
+    }
 
   }
 
+  clearAndRefreshCountInRooms() {
+    for (var room of this.RoomList) {
+      // clear count for Staff, Children, Unknowns
+      room.numOfStaff = 0;
+      room.numOfChildren = 0;
+      room.numOfUnknown = 0;
+    }
+  }
+
   createDaycareLayout() {
-    var point1 = new Point(300,100);
-    var point2 = new Point(300,250);
-    var point3 = new Point(450,250);
-    var point4 = new Point(450,100);
+    var point1 = new Point(400,100);
+    var point2 = new Point(400,250);
+    var point3 = new Point(550,250);
+    var point4 = new Point(550,100);
 
-    var point5 = new Point(450,400);
+    var point5 = new Point(550,400);
 
-    var point6 = new Point(600,650);
-    var point7 = new Point(750,600);
+    var point6 = new Point(700,650);
+    var point7 = new Point(850,600);
 
     var roomSize = new Size(150,150);
     var hallwaySize = new Size(150,400);
 
+    var room;
+
     var room1 = new Path.Rectangle(point1, roomSize);
-    room1.strokeColor = new Color('green');
+    room1.strokeColor = new Color('#3F51B5');
+    room1.strokeWidth = 5;
     room1.fillColor = new Color('white');
     room1.data.nodeID = "1";
+    room = {nodeID: room1.data.nodeID, startPoint: point1, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room2 = new Path.Rectangle(point2, roomSize);
-    room2.strokeColor = new Color('green');
+    room2.strokeColor = new Color('#3F51B5');
+    room2.strokeWidth = 5;
     room2.fillColor = new Color('white');
     room2.data.nodeID = "2";
+    room = {nodeID: room2.data.nodeID, startPoint: point2, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room3 = new Path.Rectangle(point3, roomSize);
-    room3.strokeColor = new Color('green');
+    room3.strokeColor = new Color('#3F51B5');
+    room3.strokeWidth = 5;
     room3.fillColor = new Color('white');
     room3.data.nodeID = "3";
+    room = {nodeID: room3.data.nodeID, startPoint: point3, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room4 = new Path.Rectangle(point4, roomSize);
-    room4.strokeColor = new Color('green');
+    room4.strokeColor = new Color('#3F51B5');
+    room4.strokeWidth = 5;
     room4.fillColor = new Color('white');
     room4.data.nodeID = "4";
+    room = {nodeID: room4.data.nodeID, startPoint: point4, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room5 = new Path.Rectangle(point5, hallwaySize);
-    room5.strokeColor = new Color('blue');
+    room5.strokeColor = new Color('#3F51B5');
+    room5.strokeWidth = 5;
     room5.fillColor = new Color('white');
     room5.data.nodeID = "5";
+    room = {nodeID: room5.data.nodeID, startPoint: point5, roomSize: hallwaySize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room6 = new Path.Rectangle(point6, roomSize);
-    room6.strokeColor = new Color('green');
+    room6.strokeColor = new Color('#3F51B5');
+    room6.strokeWidth = 5;
     room6.fillColor = new Color('white');
     room6.data.nodeID = "6";
+    room = {nodeID: room6.data.nodeID, startPoint: point6, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var room7 = new Path.Rectangle(point7, roomSize);
-    room7.strokeColor = new Color('green');
+    room7.strokeColor = new Color('#3F51B5');
+    room7.strokeWidth = 5;
     room7.fillColor = new Color('white');
     room7.data.nodeID = "7";
+    room = {nodeID: room7.data.nodeID, startPoint: point7, roomSize: roomSize, numOfStaff: 0, numOfChildren: 0, numOfUnknown: 0};
+    this.RoomList.push(room);
 
     var text1 = new PointText({
-      point: [350,120],
+      point: [450,120],
       content: 'Room 1',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text2 = new PointText({
-      point: [500,120],
+      point: [600,120],
       content: 'Room 2',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text3 = new PointText({
-      point: [500,270],
+      point: [600,270],
       content: 'Room 3',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
-    });
-
-    var text3 = new PointText({
-      point: [500,270],
-      content: 'Room 3',
-      fillColor: 'black',
-      fontFamily: 'sans-serif',
-      fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text4 = new PointText({
-      point: [350,270],
+      point: [450,270],
       content: 'Room 4',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text5 = new PointText({
-      point: [490,570],
+      point: [590,570],
       content: 'Hallway 1',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text6 = new PointText({
-      point: [650,670],
+      point: [750,670],
       content: 'Room 6',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
     var text7 = new PointText({
-      point: [800,620],
+      point: [900,620],
       content: 'Room 7',
       fillColor: 'black',
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
-      fontSize: 15
+      fontSize: 15,
+      locked: true
     });
 
   }
@@ -227,26 +338,44 @@ export class LivemapComponent implements OnInit {
   }
 
   refreshMap() {
+    this.clearAndRefreshCountInRooms()
     this.LastLocations = new Array();
     this.Employee.forEach(element => {
         this.locatePerson(element.name);
     });
     this.openSnackBar("Refreshing Map", "Dismiss");
+    
   }
 
-  firstMapRefresh() {
-    this.LastLocations = new Array();
-    this.Employee.forEach(element => {
-        this.locatePerson(element.name);
-    });
-    this.openSnackBar("Refreshing Map", "Dismiss");
+  /**
+   * Increment the room count based on who this person is: Staff, Student, Unknown
+   * @param person 
+   */
+  incrementRoomCount(person) {
+    for (var room of this.RoomList) {
+      if (person.nodeID == room.nodeID) {
+        // check if they are Staff, Children, or Unknown person and increment the value for that room
+        if(person.title == "admin") {
+          room.numOfStaff++;
+        } else if(person.title == "student") {
+          room.numOfChildren++;
+        } else if(person.title == "None") {
+          room.numOfUnknown++;
+        }
+      }
+    }
   }
 
   locatePerson(name) {
     this.apiService.locatePerson(name).subscribe((data) => {
       if(data.staff_id != undefined){
-        var person = {name: data.staff_id, title: data.staff_title, nodeID: data.node_id};
+        var person = {name: data.staff_id, title: data.staff_title, nodeID: data.node_id, lastSeen: data.timestamp};
         this.LastLocations.push(person);
+
+        // increment the RoomList with knowledge of this person
+        this.incrementRoomCount(person);
+        this.drawPeopleOnMap();
+
       }
     })
   }
